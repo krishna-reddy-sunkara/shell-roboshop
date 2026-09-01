be7f2e8ec8be7dc73fa784956ee59d4f51837e69
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-01 14:29:11 +0530

    actions

--- 01-roboshop.sh
+++ 01-roboshop.sh
@@ -5,9 +5,9 @@ SG_ID="sg-0027bf26309cfab1a"
 for instance in $@
 do
 aws ec2 run-instances \
-    --image-id AMI_ID \
+    --image-id $AMI_ID \
     --instance-type t3.micro \
-    --security-group-ids SG_ID \
+    --security-group-ids $SG_ID \
     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyInstanceName}]' \
     --query 'Instances[0].PrivateIpAddress' \
     --output text
