3678902f8587d77a6d2117bc720cd7466ab14c7d
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-01 23:03:22 +0530

    actions

---
+++ mongodb.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FLODER="/var/log/shell-roboshop"
+LOGS_FILE="LOGS_FLODER/$0.log"
+
+  if [ $USERID -ne 0 ]; then
+   echo " please run this script with sudo user " | tee -a $LOGS_FILE
+     exit 1
+  fi 
+    mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $? -ne 0 ]; then
+    echo " $2 .... failure " | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo " $2 .... success " | tee -a $LOGS_FILE
+    fi
+}   
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "copying the repo"
+
+dnf install mongodb-org -y 
+VALIDATE $? "installing mongodb"
+
+systemctl enable mongod 
+VALIDATE $? "enabling mongodb"
+
+systemctl start mongod 
+VALIDATE $? "starring mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "allowing remote connections"
+
+systemctl restart mongod
+VALIDATE $? "restarting mongod"
