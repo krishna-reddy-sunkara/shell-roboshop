f0d301722e0d5cbb6d8c2a032c49eaf4125be991
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-01 12:47:03 +0530

    actions

---
+++ 01-roboshop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+AMI_ID="ami-0220d79f3f480ecf5"
+SG_ID="sg-0027bf26309cfab1a"
+INS_TYPE="t3.micro"
+
+for instance in $@
+
+do
+aws ec2 run-instances \
+    --image-id AMI_ID \
+    --instance-type INS_TYPE \
+    --security-group-ids SG_ID \
+    --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=MyInstanceName}]' \
+    --query 'Instances[0].PrivateIpAddress' \
+    --output text
+    
+done
