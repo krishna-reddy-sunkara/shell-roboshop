4d652010b345f7db56acb4fadf17a6d8866d11e9
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-02 14:19:40 +0530

    actions

--- 01-catalogue.sh
+++ 01-catalogue.sh
@@ -55,7 +55,7 @@ VALIDATE(){
      npm install 
      VALIDATE $? "downloeading depencies" 
 
-     cp /home/ec2-user/shell-roboshop/catalogue.service /etc/systemd/system/catalogue.service
+     cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
      VALIDATE $? "coping catalogue.service to catalogue"
 
      systemctl daemon-reload
