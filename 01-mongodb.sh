64fea707eb2735aa519f7572c67e4245fd5d82c3
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-02 12:02:57 +0530

    actions

--- 01-mongodb.sh
+++ 01-mongodb.sh
@@ -22,17 +22,17 @@ VALIDATE(){
     cp mongo.repo /etc/yum.repos.d/mongo.repo
     VALIDATE $? "coping mongo.repo "
 
-dnf install mongodb-org -y &>>$LOGS_FILE
+dnf install mongodb-org -y
 VALIDATE $? "installing mongodb "
 
-systemctl enable mongod &>>$LOGS_FILE
+systemctl enable mongod 
 VALIDATE $? "enabling mongodb "
 
-systemctl start mongod &>>$LOGS_FILE
+systemctl start mongod 
 VALIDATE $? "starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "allowing remote connections"
  
-systemctl restart mongod &>>$LOGS_FILE
+systemctl restart mongod 
 VALIDATE $? "restarting mongodb"
