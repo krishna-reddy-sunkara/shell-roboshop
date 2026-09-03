ce8590cc2dd3d35ecd006f240fa7acd32bcca7a6
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 15:29:59 +0530

    actions

--- 01-catalogue.sh
+++ 01-catalogue.sh
@@ -98,7 +98,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws-92s.store
+MONGODB_HOST="mongodb.daws-92s.store"
 
 if [ $USERID -ne 0 ]; then
     echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
--- 01-rabbitmq.sh
+++ 01-rabbitmq.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 SCRIPT_DIR=$PWD
-MYSQL_HOST=mysql.daws-92s.store
+MYSQL_HOST="mysql.daws-92s.store"
 
   if [ $USERID -ne 0 ]; then
    echo " please run this script with sudo shipping " | tee -a $LOGS_FILE
@@ -46,4 +46,3 @@ id roboshop
    rabbitmqctl add_user roboshop roboshop123
    rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
    VALIDATE $? "created user to rabbitmq"
-   
