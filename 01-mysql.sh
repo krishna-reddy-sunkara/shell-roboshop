9ab1483797695af6246727848e28cad8e58631a1
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 10:49:52 +0530

    actions

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -1,31 +1,34 @@
+
 #!/bin/bash
 
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-SCRIPT_DIR=$PWD
-MONGODB_HOST="mongodb.daws-92s.store"
 
-  if [ $USERID -ne 0 ]; then
-   echo " please run this script with sudo user " | tee -a $LOGS_FILE
-     exit 1
-  fi 
-    mkdir -p $LOGS_FOLDER
+
+if [ $USERID -ne 0 ]; then
+    echo  " Please run this script with root user access " | tee -a $LOGS_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo " $2 .... failure " | tee -a $LOGS_FILE
-    exit 1
-    else 
-    echo " $2 .... success " | tee -a $LOGS_FILE
+        echo -e "$2 ...  FAILURE  | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ...  SUCCESS " | tee -a $LOGS_FILE
     fi
-}   
+}
 
-dnf install mysql-server -y
-VALIDATE $? "installing mysql"
+dnf install mysql-server -y &>>$LOGS_FILE
+VALIDATE $? "Install MySQL server"
 
-systemctl enable mysqld
+systemctl enable mysqld &>>$LOGS_FILE
 systemctl start mysqld  
-VALIDATE $? "enabling and diabling"
+VALIDATE $? "Enable and start mysql"
 
+# get the password from user
 mysql_secure_installation --set-root-pass RoboShop@1
-VALIDATE $? "set the root password"
+VALIDATE $? "Setup root password"
