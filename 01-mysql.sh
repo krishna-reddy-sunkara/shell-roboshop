77a86071a58f9b3f19079b97acc4b4e1062984f2
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 10:53:58 +0530

    actions

--- 01-mysql.sh
+++ 01-mysql.sh
@@ -15,7 +15,7 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ...  FAILURE  | tee -a $LOGS_FILE
+        echo -e "$2 ...  FAILURE " | tee -a $LOGS_FILE
         exit 1
     else
         echo -e "$2 ...  SUCCESS " | tee -a $LOGS_FILE
