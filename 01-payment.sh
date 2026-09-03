4d309b7c81e260162dfeff6f0182297cc845396f
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 11:46:04 +0530

    actions

---
+++ 01-payment.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+MONGODB_HOST="mongodb.daws-92s.store"
+
+  if [ $USERID -ne 0 ]; then
+   echo " please run this script with sudo user " | tee -a $LOGS_FILE
+     exit 1
+  fi 
+    mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo " $2 .... failure " | tee -a $LOGS_FILE
+    exit 1
+    else 
+    echo " $2 .... success " | tee -a $LOGS_FILE
+    fi
+}   
+
+dnf install python3 gcc python3-devel -y
+VALIDATE $? "installing paython "
+
+useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+VALIDATE $? "creating system user"
+
+mkdir -p /app 
+VALIDATE $? "creating app folder"
+
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zi
+VALIDATE $? "Downlpeading code"
+
+cd /app 
+VALIDATE $? "Moving app directory"
+
+rm -rf /app/*
+VALIDATE $? "romoving data from app"
+
+unzip /tmp/payment.zip
+VALIDATE $? "unzing the code"
+
+cd /app 
+pip3 install -r requirements.txt
+VALIDATE $? "change to app directory"
+
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
+VALIDATE $? "Copying payment.service to payment"
+
+systemctl daemon-reload
+VALIDATE $? "Deamon reloaded"
+
+systemctl enable payment 
+systemctl start payment
+VALIDATE $? "enabling and starting payment"
+
+
