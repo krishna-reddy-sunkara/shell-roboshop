f81e51120ab1090d630f7324e581309118789091
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 13:27:55 +0530

    actions

--- 01-frontend.sh
+++ 01-frontend.sh
@@ -4,7 +4,7 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
-SCRIPT_DIR=$PWD
+SCRIPT_DIR="$PWD"
 
   if [ $USERID -ne 0 ]; then
    echo " please run this script with sudo user " | tee -a $LOGS_FILE
