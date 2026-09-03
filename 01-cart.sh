961ab134b0abe8ac7fc2f9d877b0536686b015b1
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-03 14:36:07 +0530

    actions

--- 01-cart.sh
+++ 01-cart.sh
@@ -3,64 +3,67 @@
 USERID=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 SCRIPT_DIR=$PWD
-MONGODB_HOST="mongodb.daws-92s.store"
+MONGODB_HOST=mongodb.daws-92s.store
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    exit 1
+fi
 
-  if [ $USERID -ne 0 ]; then
-   echo " please run this script with sudo user " | tee -a $LOGS_FILE
-     exit 1
-  fi 
-    mkdir -p $LOGS_FOLDER
+mkdir -p $LOGS_FOLDER
+
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-    echo " $2 .... failure " | tee -a $LOGS_FILE
-    exit 1
-    else 
-    echo " $2 .... success " | tee -a $LOGS_FILE
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
-}   
-    dnf module disable nodejs -y &>>$LOGS_FILE
-    VALIDATE $? "disabling nodejs"
+}
 
-    dnf module enable nodejs:20 -y &>>$LOGS_FILE
-    VALIDATE $? "Enabling nodejs"
+dnf module disable nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Disabling NodeJS Default version"
 
-    dnf install nodejs -y &>>$LOGS_FILE
-    VALIDATE $? "installing nodejs"
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
+VALIDATE $? "Enabling NodeJS 20"
 
-    id roboshop
-    if [ $? -ne 0 ]; then
-         useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-        VALIDATE $? "Creating system user"
+dnf install nodejs -y &>>$LOGS_FILE
+VALIDATE $? "Install NodeJS"
 
-   else 
-         echo "user already exits .... skipping "
-   fi
+id roboshop &>>$LOGS_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
-    mkdir -p /app 
-    VALIDATE $? "creating app folder"
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
 
-    curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip 
-    VALIDATE $? "Downlpeading code"
+curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip  &>>$LOGS_FILE
+VALIDATE $? "Downloading cart code"
 
-     cd /app 
-     VALIDATE $? "Moving app directory"
+cd /app
+VALIDATE $? "Moving to app directory"
 
-     rm -rf /app/*
-     VALIDATE $? "romoving data from app"
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
 
-     unzip /tmp/cart.zip
-     VALIDATE $? "unzing the code"
+unzip /tmp/cart.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip cart code"
 
-     npm install 
-     VALIDATE $? "downloeading depencies"
+npm install  &>>$LOGS_FILE
+VALIDATE $? "Installing dependencies"
 
-     cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
-     VALIDATE $? "copying services"
-     
-     systemctl daemon-reload
-     VALIDATE $? "daemon realoading"
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+VALIDATE $? "Created systemctl service"
 
-     systemctl enable cart 
-     systemctl start cart
-     VALIDATE $? "enabling and starting cart"
+systemctl daemon-reload
+systemctl enable cart  &>>$LOGS_FILE
+systemctl start cart
+VALIDATE $? "Starting and enabling cart"
