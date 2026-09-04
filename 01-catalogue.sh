28dd8b00106eddc53f6c3fa1d102f23226959c7d
krishna reddy s <sunkarakrishnareddy9@gmail.com>
2026-09-04 14:52:32 +0530

    actions

--- 01-catalogue.sh
+++ 01-catalogue.sh
@@ -1,62 +1,59 @@
 #!/bin/bash
 
-USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FOLDER="/var/log/roboshop"
+sudo mkdir -p $LOGS_FOLDER
+sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
+sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+
+USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-SCRIPT_DIR=$PWD
-MONGODB_HOST=mongodb.daws-92s.store
+TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 if [ $USERID -ne 0 ]; then
-    echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+    echo -e "$TIMESTAMP [ERROR] $R Please run this script with root access $N" | tee -a $LOGS_FILE
     exit 1
 fi
 
-mkdir -p $LOGS_FOLDER
-
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [ERROR] $2 ... $R FAILURE $N" | tee -a $LOGS_FILE
         exit 1
     else
-        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+        echo -e "$TIMESTAMP [INFO] $2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
     fi
 }
 
 dnf module disable nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Disabling NodeJS Default version"
-
-dnf module enable nodejs:20 -y &>>$LOGS_FILE
-VALIDATE $? "Enabling NodeJS 20"
-
+dnf module enable nodejs:20 -y  &>>$LOGS_FILE
 dnf install nodejs -y &>>$LOGS_FILE
-VALIDATE $? "Install NodeJS"
+VALIDATE $? "Installing NodeJS:20"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-    VALIDATE $? "Creating system user"
+    VALIDATE $? "Creating roboshop system user"
 else
-    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
 fi
 
-mkdir -p /app 
+rm -rf /app
+VALIDATE $? "Removing existing code"
+
+rm -rf /tmp/catalogue.zip
+VALIDATE $? "Removed catalogue zip"
+
+mkdir -p /app  &>>$LOGS_FILE
 VALIDATE $? "Creating app directory"
 
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip  &>>$LOGS_FILE
-VALIDATE $? "Downloading catalogue code"
-
-cd /app
-VALIDATE $? "Moving to app directory"
-
-rm -rf /app/*
-VALIDATE $? "Removing existing code"
-
+cd /app 
 unzip /tmp/catalogue.zip &>>$LOGS_FILE
-VALIDATE $? "Uzip catalogue code"
+VALIDATE $? "Downloaded and extracted catalogue code"
 
 npm install  &>>$LOGS_FILE
 VALIDATE $? "Installing dependencies"
@@ -64,22 +61,21 @@ VALIDATE $? "Installing dependencies"
 cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 VALIDATE $? "Created systemctl service"
 
-systemctl daemon-reload
-systemctl enable catalogue  &>>$LOGS_FILE
-systemctl start catalogue
-VALIDATE $? "Starting and enabling catalogue"
-
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Added Mongo repo" 
+
 dnf install mongodb-mongosh -y &>>$LOGS_FILE
+VALIDATE $? "Installed MongoDB client"
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet  --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+INDEX=$(mongosh --host mongodb.daws-92s.store --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
 
-if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js
-    VALIDATE $? "Loading products"
+if [ $INDEX -lt 0 ]; then
+    mongosh --host mongodb.daws-92s.store </app/db/master-data.js &>>$LOGS_FILE
+    VALIDATE $? "Load Products"
 else
     echo -e "Products already loaded ... $Y SKIPPING $N"
 fi
 
-systemctl restart catalogue
+systemctl enable catalogue &>>$LOGS_FILE
+systemctl restart catalogue &>>$LOGS_FILE
 VALIDATE $? "Restarting catalogue"
